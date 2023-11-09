bf96b1f8c175aa3b1033b1a7e767fb98211ec735
Sricharan2211 <tudugani.charan@mphasis.com>
2023-11-09 15:29:57 +0530

    day 4 code 1

---
+++ Assignment_5/Assignment_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Charan";
+            int age = 23; 
+            double height = 175.5; 
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height + " cm");
+            Console.ReadKey();
+        }
+    }
+}
